73e917076fdcd7b7761e30d3580b16843f34b849
omarrodriguez122005 <90008968+omarrodriguez122005@users.noreply.github.com>
2021-09-18 03:45:28 -0500

    Update enviar.php

--- php/enviar.php
+++ php/enviar.php
@@ -1,14 +1,26 @@
 <?php
-$destinatario = 'orodriguezhcoarcaj@gmail.com'; 
+
 $nombre = $_POST['nombre'];
 $correo = $_POST['correo'];
 $telefono = $_POST['telefono'];
 $mensaje = $_POST['mensaje'];
 
-$header = "Enviado desde la página CHE CARLITOS RESTAURANT"
-$mensajeCompleto = $mensaje . "\nAtentamente: " . $nombre;
+$header = 'From= ' . $correo . " \r\n";
+$header .= "X-Mailer: PHP/" . phpversion() . " \r\n";
+$header .= "Mime-Version: 1.0 \r\n";
+$header .= "Content-Type: text/plain";
 
-mail($destinatario, $correo, $mensajeCompleto, $header);
-echo "<script>alert('Correo enviado exitosamente') </script>";
-echo "<script> setTimeout(\"location.href='index.html'\",1000)</script>";
+$mensaje = "Este mensaje fue enviado por: " . $nombre . " \r\n";
+$mensaje .= "Su e-mail es: " . $correo . " \r\n";
+$mensaje .= "Teléfono de contacto: " . $telefono . " \r\n";
+$mensaje .= "Mensaje: " . $_POST['mensaje'] . " \r\n";
+$mensaje .= "Enviado el: " . date('d/m/y', time());
+
+$para = 'orodriguezhcoarcaj@gmail.com';
+$asunto = 'CONTACTO';
+
+mail($para, $asunto, utf8_decode($mensaje), $header); 
+
+header("Location:index.html");
+
 ?>
